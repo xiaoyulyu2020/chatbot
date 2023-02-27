b6f5ce983e082cbbcbabc09aa2985cf663eb1f7b
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2023-02-27 14:11:25 +0000

    change

--- app/chatbot/urls.py
+++ app/chatbot/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-
+    path('main/', views.chatbot, name="main"),
 ]
--- app/chatbot/views.py
+++ app/chatbot/views.py
@@ -11,5 +11,16 @@ api_key = os.getenv("OPENAI_KEY", None)
 def chatbot(request):
     chatbot_response = None
     if api_key is not None and request.method == "POST":
+        openai.api_key = api_key
         user_input = request.POST.get('user_input')
-    return render(request, "main.html", {})
+        prompt = user_input
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=prompt,
+            max_tokens=256,
+            stop='.',
+            temperature=0.5,
+        )
+        print(response)
+        chatbot_response = response['choices'][0]['text']
+    return render(request, "app/main.html", {'response': chatbot_response})
